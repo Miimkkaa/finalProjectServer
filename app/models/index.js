1644b0816e4649b2db1e83ba49db9ef702e4c462
Miimkkaa <mile0416@edu.easj.dk>
2020-04-28 11:46:13 +0200

    hey

---
+++ app/models/index.js
@@ -0,0 +1,15 @@
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize('testdb', 'root', '', {
+  host: "localhost",
+  dialect: "mysql",
+  port: 3306
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
+
+module.exports = db;
